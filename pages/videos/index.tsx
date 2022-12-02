aa1f1ba45c19e8a17923b30a803f43e249558f4b
quangquandev <quan.tran@pionero.io>
2022-12-03 02:10:32 +0700

    hot fix

---
+++ pages/videos/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const index = (props: Props) => {
+  return (
+    <video width="320" height="240" controls>
+      <source src="https://da0lvxjpdqmhr.cloudfront.net/public/mixkit-portrait-of-a-catrina-with-a-candle-and-smoke-in-44259-medium.mp4" type="video/mp4" />
+    </video>
+  );
+};
+
+export default index;
